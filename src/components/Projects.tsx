1b6edcdf389fbecc0b729aa4c845ec889955a7ec
Karn_Gupta <karnguptaprivate123@gmail.com>
2025-03-12 19:51:30 +0530

    Small Update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -63,7 +63,7 @@ const Projects = () => {
 
         <div className="mt-16 text-center animate-fade-in">
           <a
-            href="#"
+            href="https://github.com/Rag3karn"
             className="inline-flex items-center text-blue-600 hover:text-blue-700 font-medium transition-colors"
           >
             View All Projects
